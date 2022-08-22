9831e03e2cc411174434bf3490607e99d809ec4c
Cristian Sanchez <crisey937@gmail.com>
2022-08-22 00:59:42 -0500

    Completed videos A and B

---
+++ app/src/main/java/com/example/fragments/ItemListFragment.kt
@@ -0,0 +1,32 @@
+package com.example.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ItemListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_item_list, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ItemListFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) = ItemListFragment()
+    }
+}
--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -2,10 +2,46 @@ package com.example.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.ListFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navBar : BottomNavigationView
+
+    private lateinit var newItemFragment: NewTaskFragment
+    private lateinit var itemListFragment: ItemListFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        newItemFragment = NewTaskFragment.newInstance()
+        itemListFragment = ItemListFragment.newInstance()
+
+        navBar = findViewById(R.id.navBar)
+
+        showFragment(itemListFragment)
+
+        navBar.setOnItemSelectedListener { menuItem ->
+            if(menuItem.itemId == R.id.newItem) {
+                showFragment(newItemFragment)
+            }else if(menuItem.itemId == R.id.itemList) {
+                showFragment(itemListFragment)
+            }
+
+            true
+        }
+
     }
+
+    fun showFragment(fragment : Fragment) {
+
+        val transaction = supportFragmentManager.beginTransaction()
+
+        transaction.replace(R.id.fragmentLayout, fragment)
+        transaction.commit()
+    }
+
 }
